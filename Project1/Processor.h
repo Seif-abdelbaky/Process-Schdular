693a5e92d5796953e9e004c4cd007e4a15ad4506
Seif-abdelbaky <seifabdelbaky2211@gmail.com>
2023-03-26 16:05:03 +0200

    Added "Processor base abstract class"

---
+++ Project1/Processor.h
@@ -0,0 +1,19 @@
+#pragma once
+#include"QueueADT.h"
+#include"process.h"
+class Processor
+{
+	int TimeLeftInQueue;
+	Process* runPtr;
+	QueueADT<Process>* readyQ;
+	bool busy;
+public:
+	void virtual ScheduleAlgo()=0;
+	void virtual Run() = 0;
+	void virtual setBusy(bool b) = 0;
+	bool virtual isBusy() = 0;
+	void virtual setTimeLeftInQueue(int t) = 0;
+	int virtual getTimeLeftInQueue() = 0;
+	virtual QueueADT<Process>*  getReadyQ() = 0;
+};
+
